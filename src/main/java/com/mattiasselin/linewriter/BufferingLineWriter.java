90f5268fc3203d182d569593471430c05520a509
Mattias Selin <43535744+MattiasSelinEyevinn@users.noreply.github.com>
2021-02-03 16:16:52 +0100

    Update BufferingLineWriter.java

--- src/main/java/com/mattiasselin/linewriter/BufferingLineWriter.java
+++ src/main/java/com/mattiasselin/linewriter/BufferingLineWriter.java
@@ -1,10 +1,9 @@
+ package com.mattiasselin.linewriter;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import com.mattiasselin.linewriter.ILineSource;
-import com.mattiasselin.linewriter.ILineWriter;
 
 public class BufferingLineWriter implements ILineWriter, ILineSource {
 	private final IIdGenerator idGenerator;
